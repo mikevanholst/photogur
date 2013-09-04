eb286d84f435ec6d658799168378e376ce48fa5d
mikevanholst <admin@admins-MacBook-Pro.local>
2013-09-04 17:09:43 -0400

    individual pictures now show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Photogur::Application.routes.draw do
 
 get 'pictures' => 'pictures#index'
 
+get 'pictures/0' => 'pictures#picture0'
+get 'pictures/1' => 'pictures#picture1'
+get 'pictures/2' => 'pictures#picture2'
+
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
